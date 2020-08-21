77e8be7ad0ce92f4b0340d2005fcb44eed1f164b
JoseJaico14 <jaico3456789@gmail.com>
2020-08-21 01:11:17 -0500

    primer commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.use(VueAxios,axios)
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,142 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <b-container>
+      <b-row class="justify-content-md-start mb-3">
+        <b-col col lg="1">
+        </b-col>
+        <b-col col lg="3">
+          <b-form-select v-model="selected" :options="options" @change="changstate(selected)"></b-form-select>
+        </b-col>
+        <b-col col lg="1">
+          <b-button variant="primary" @click="reset()"><b-icon icon="arrow-counterclockwise" animation="spin-reverse" font-scale="1"></b-icon></b-button>
+        </b-col>
+        <b-col col lg="1">
+        </b-col>
+        <b-col col lg="3">
+          <b-form @submit.prevent="filter">
+            <b-form-input v-model="text" placeholder="Nombre o Email y Enter"></b-form-input>
+          </b-form>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container>
+      <b-row class="justify-content-md-center">
+        <b-col col lg="10">
+          <b-table striped hover :items="items"></b-table>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container>
+		  <b-row class="justify-content-md-center">
+        <b-col col lg="6">
+          <b-pagination
+              v-model="currentPage"
+              :total-rows="rows"
+              :per-page="perPage"
+              class="mt-4"
+          >
+            <template v-slot:first-text><span class="text-success" @click="paginate(first)">Primero</span></template>
+            <template v-slot:prev-text><span class="text-danger" @click="paginate(prev)">Antes</span></template>
+            <template v-slot:next-text><span class="text-warning" @click="paginate(next)">Siguiente</span></template>
+            <template v-slot:last-text><span class="text-info" @click="paginate(last)">Ultimo</span></template>
+            <template v-slot:ellipsis-text>
+              <b-spinner small type="grow"></b-spinner>
+              <b-spinner small type="grow"></b-spinner>
+              <b-spinner small type="grow"></b-spinner>
+            </template>
+            <template v-slot:page="{ page, active }">
+              <span @click="PagiNum(page)">
+              <b v-if="active">{{ page }}w</b>
+              <i v-else>{{ page }}t</i>
+              </span>
+            </template>
+          </b-pagination>
+        </b-col>
+	    </b-row>
+	  </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  import axios from 'axios'
+  export default {
+    data() {
+      return {
+        items: [
+          
+        ],
+        selected: null,
+        text: '',
+        options: [
+          { value: null, text: 'Selecciona una opción' },
+          { value: '1', text: 'Activos' },
+          { value: '0', text: 'Desactivos' }
+        ],
+        errors:[],
+        rows: 1,
+        perPage: 1,
+        currentPage:1 ,
+        first:'',
+        last:'',
+        prev:'',
+        next:'',
+        page_url:'/user/all',
+		    number_page:'/user/all?page=',
+      }
+    },
+    methods:{
+        filter(){
+          axios.get(`/user/search/${this.text}`).then((response)=>{
+            this.number_page = `/user/search/${this.text}?page=`;
+            this.parsePagination(response);
+          }).catch((error)=>{
+            console.log(error);
+          })
+        },
+        listUsers(){
+          axios.get(this.page_url).then((response)=>{
+            this.parsePagination(response);
+          }).catch((error)=>{
+            console.log(error);
+          });
+        },
+        paginate(page_url){
+          this.page_url = page_url;
+          this.listUsers();
+        },
+        PagiNum(page){
+          this.page_url = this.number_page+(parseInt(page));
+          this.listUsers();
+        },
+        changstate(state){
+          axios.get(`/user/status/${state}`)
+          .then((response)=>{
+            this.number_page = `/user/status/${state}?page=`;
+            this.parsePagination(response);
+          }).catch((error)=>{
+            console.log(error)
+          });
+        },
+        parsePagination(response){
+          this.rows = response.data.total;
+          this.perPage = response.data.per_page;
+          this.currentPage = response.data.current_page;
+          this.first = response.data.first_page_url;
+          this.last = response.data.last_page_url;
+          this.prev = response.data.prev_page_url;
+          this.next = response.data.next_page_url;
+          this.items=response.data.data;
+        },
+        reset(){
+          this.page_url='/user/all';
+          this.number_page='/user/all?page=';
+          this.selected=null;
+          this.text="";
+          this.listUsers();
+        }
+    },
+    created(){
+      this.listUsers();
+    }
   }
-}
 </script>
